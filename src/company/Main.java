d8fc83caabfcf6e6ca4e240580e3900eebe5111d
DANA CUZMICIOV <dudukuzmiciov@gmail.com>
2022-05-15 11:24:41 -0400

    the basic structure of a car

---
+++ src/company/Main.java
@@ -0,0 +1,15 @@
+package company;
+
+public class Main {
+    public static void main(String[] args) {
+        basicFeatures Audii =new basicFeatures();
+        Audii.Color="pink";
+        Audii.regNo="BMW";
+        Audii.headlights="red";
+        Audii.engineCapac="v12";
+        Audii.doors="7";
+
+
+    }
+
+}
