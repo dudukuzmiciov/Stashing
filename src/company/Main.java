1c6a8110ed5428221c1fcdc3c1200e7b3b9a56a6
DANA CUZMICIOV <dudukuzmiciov@gmail.com>
2022-05-17 16:59:23 -0400

    the basic feature of the car is ready, working on advance features

--- src/company/Main.java
+++ src/company/Main.java
@@ -2,13 +2,9 @@ package company;
 
 public class Main {
     public static void main(String[] args) {
-        basicFeatures Audii =new basicFeatures();
-        Audii.Color="pink";
-        Audii.regNo="BMW";
-        Audii.headlights="red";
-        Audii.engineCapac="v12";
-        Audii.doors="7";
-
+//create the object of basic features:
+        basicFeatures CAR =  new basicFeatures();
+        
 
     }
 
